19fc41fbfbfa0a414cf0dcf584894057c21f9209
omatica <ian@omatica>
2025-01-21 22:47:37 +0000

    mode config tweaked. fzfl-lua filename first

--- nvim/.config/nvim/lua/custom/plugins/modes.lua
+++ nvim/.config/nvim/lua/custom/plugins/modes.lua
@@ -1,32 +1,32 @@
 return {
-  'mvllow/modes.nvim',
-  tag = 'v0.2.0',
-  config = function()
-    require('modes').setup {
-      colors = {
-        bg = '', -- Optional bg param, defaults to Normal hl group
-        copy = '#ffaa88',
-        delete = '#c75c6a',
-        insert = '#789978',
-        visual = '#b69bf1',
-      },
+	"mvllow/modes.nvim",
+	tag = "v0.2.1",
+	config = function()
+		require("modes").setup({
+			colors = {
+				bg = "", -- Optional bg param, defaults to Normal hl group
+				copy = "#ffaa88",
+				delete = "#c75c6a",
+				insert = "#789978",
+				visual = "#b69bf1",
+			},
 
-      -- Set opacity for cursorline and number background
-      line_opacity = 0.15,
+			-- Set opacity for cursorline and number background
+			line_opacity = 0.25,
 
-      -- Enable cursor highlights
-      set_cursor = false,
+			-- Enable cursor highlights
+			set_cursor = false,
 
-      -- Enable cursorline initially, and disable cursorline for inactive windows
-      -- or ignored filetypes
-      set_cursorline = true,
+			-- Enable cursorline initially, and disable cursorline for inactive windows
+			-- or ignored filetypes
+			set_cursorline = true,
 
-      -- Enable line number highlights to match cursorline
-      set_number = true,
+			-- Enable line number highlights to match cursorline
+			set_number = true,
 
-      -- Disable modes highlights in specified filetypes
-      -- Please PR commonly ignored filetypes
-      ignore_filetypes = { 'NvimTree', 'TelescopePrompt' },
-    }
-  end,
+			-- Disable modes highlights in specified filetypes
+			-- Please PR commonly ignored filetypes
+			ignore_filetypes = { "NvimTree", "TelescopePrompt" },
+		})
+	end,
 }
--- nvim/.config/nvim/lua/custom/plugins/neogit.lua
+++ nvim/.config/nvim/lua/custom/plugins/neogit.lua
@@ -1,26 +1,45 @@
 return {
-  'NeogitOrg/neogit',
+	"NeogitOrg/neogit",
 
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'sindrets/diffview.nvim',
-    -- 'nvim-telescope/telescope.nvim',
-    'ibhagwan/fzf-lua',
-  },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		-- 'nvim-telescope/telescope.nvim',
+		"ibhagwan/fzf-lua",
+	},
 
-  config = function()
-    vim.keymap.set('n', '<leader>g', ':Neogit kind=replace<CR>', { noremap = true, silent = true, desc = 'Open Neogit' })
-    vim.keymap.set('n', '<leader>gs', ':Neogit kind=split<CR>', { noremap = true, silent = true, desc = 'Open Neogit in split' })
-    vim.keymap.set('n', '<leader>gb', ':FzfLua git_branches<CR>', { noremap = true, silent = true, desc = 'Search branches' })
-    vim.keymap.set('n', '<leader>gc', ':FzfLua git_commits<CR>', { noremap = true, silent = true, desc = 'Search commits' })
+	config = function()
+		vim.keymap.set(
+			"n",
+			"<leader>g",
+			":Neogit kind=replace<CR>",
+			{ noremap = true, silent = true, desc = "Open Neogit" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gs",
+			":Neogit kind=split<CR>",
+			{ noremap = true, silent = true, desc = "Open Neogit in split" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gb",
+			":FzfLua git_branches<CR>",
+			{ noremap = true, silent = true, desc = "Search branches" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>gc",
+			":FzfLua git_commits<CR>",
+			{ noremap = true, silent = true, desc = "Search commits" }
+		)
 
-    require('neogit').setup {
-      integrations = {
-        diffview = true,
-        -- telescope = true,
-        fzf_lua = true,
-      },
-      graph_style = 'kitty',
-    }
-  end,
+		require("neogit").setup({
+			integrations = {
+				diffview = true,
+				fzf_lua = true,
+			},
+			graph_style = "kitty",
+		})
+	end,
 }
