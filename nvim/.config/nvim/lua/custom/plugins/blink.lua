ba2f27d2b4c1b19d021ee07f845019b324ae4866
omatica <ian@omatica>
2025-04-26 19:33:24 +0100

    blink font fix

--- nvim/.config/nvim/lua/custom/plugins/blink.lua
+++ nvim/.config/nvim/lua/custom/plugins/blink.lua
@@ -30,7 +30,7 @@ return {
             use_nvim_cmp_as_default = true,
             -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
             -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = "mono",
+            nerd_font_variant = "normal",
         },
         completion = {
             -- 'prefix' will fuzzy match on the text before the cursor
