22277abd386bb21cdc6e7edf1622a6a5929e8a78
omatica <ian@omatica>
2025-10-29 10:07:31 +0000

    updated fish theme config

---
+++ fish/.config/fish/conf.d/theme.fish
@@ -0,0 +1,26 @@
+set -g fish_color_normal normal
+set -g fish_color_command F4F4F4
+set -g fish_color_quote 666A80
+set -g fish_color_redirection FAFAFA
+set -g fish_color_end 969696
+set -g fish_color_error FFA779
+set -g fish_color_param A0A0F0
+set -g fish_color_comment B0B0B0
+set -g fish_color_match --background=brblue
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_history_current --bold
+set -g fish_color_operator 00a6b2
+set -g fish_color_escape 00a6b2
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_valid_path --underline
+set -g fish_color_autosuggestion 9C9C9C
+set -g fish_color_user brgreen
+set -g fish_color_host normal
+set -g fish_color_cancel --reverse
+set -g fish_pager_color_prefix normal --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description B3A06D
+set -g fish_pager_color_selected_background --background=brblack
