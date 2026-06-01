d59ff4d843c17a634a96b1a6a3088859e28c3e95
omatica <ian@submethod.dev>
2026-06-01 21:58:38 +0100

    fix: live_grep_blob replaced with live_grep

--- nvim/.config/nvim/lua/custom/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/custom/plugins/fzf-lua.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>sf", ":FzfLua files<CR>", { silent = true, desc = "[S]earch [F]iles" })
         vim.keymap.set("n", "<leader>sh", ":FzfLua highlights<CR>", { silent = true, desc = "[S]earch [H]ighlights" })
-        vim.keymap.set("n", "<leader>sg", ":FzfLua live_grep_glob<CR>", { silent = true, desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sg", ":FzfLua live_grep<CR>", { silent = true, desc = "[S]earch by [G]rep" })
         vim.keymap.set("n", "<leader>sr", ":FzfLua resume<CR>", { silent = true, desc = "[S]earch [R]esume" })
         vim.keymap.set("n", "<leader>sk", ":FzfLua keymaps<CR>", { silent = true, desc = "[S]earch [K]eymaps" })
         vim.keymap.set(
